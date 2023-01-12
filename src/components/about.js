044be8bdc15834ffc4bce16efa0f468648361941
llleeeaaannn <piercefrank10@gmail.com>
2023-01-12 06:30:23 +0000

    Added further content

---
+++ src/components/about.js
@@ -0,0 +1,14 @@
+import styles from '../styles/about.module.css';
+
+const About = () => {
+
+  return (
+    <div className={styles.about}>
+      <h2>About Me</h2>
+      <h3>I love building stuff</h3>
+      <p>I recently graudated from Dublin City University with a Bachelors of Science in Enterprise Computing. I have spent the past months learnig about front-end development while creating web applications with real-world uses.</p>
+    </div>
+  )
+}
+
+export default About;
