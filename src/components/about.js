8063dc78e93b051e839bcfb2f05945213ec348c1
llleeeaaannn <piercefrank10@gmail.com>
2023-01-12 17:50:48 +0000

    Completed about styling:

--- src/components/about.js
+++ src/components/about.js
@@ -6,7 +6,7 @@ const About = () => {
     <div className={styles.about}>
       <h2>About Me</h2>
       <h3>I love building stuff</h3>
-      <p>I recently graudated from Dublin City University with a Bachelors of Science in Enterprise Computing. I have spent the past months learnig about front-end development while creating web applications with real-world uses.</p>
+      <p>I recently graudated from Dublin City University with a Bachelors of Science in Enterprise Computing. I have spent the past months learnig about front-end development while creating web applications with real-world uses. I recently graudated from Dublin City University with a Bachelors of Science in Enterprise Computing. I have spent the past months learnig about front-end development while creating web applications with real-world uses. I recently graudated from Dublin City University with a Bachelors of Science in Enterprise Computing. I have spent the past months learnig about front-end development while creating web applications with real-world uses.</p>
     </div>
   )
 }
