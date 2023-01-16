2df35edb297f05e00572123b1c25447d3b3e59d9
llleeeaaannn <piercefrank10@gmail.com>
2023-01-16 23:33:34 +0000

    Finished menu styling and functionality

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,6 +1,10 @@
+import { useContext } from 'react';
+import { AppContext } from '../App';
 import styles from '../styles/menu.module.css';
 
 const Menu = () => {
+
+  const { showMenu, setShowMenu } = useContext(AppContext);
 
   return (
     <div className={styles.menu}>
@@ -8,6 +12,7 @@ const Menu = () => {
         <div className={styles.hello}>
           <h3>SAY HELLO</h3>
           <a href="https://github.com/llleeeaaannn" rel="noopener noreferrer" target="_blank">GitHub</a>
+          <a href="https://github.com/llleeeaaannn" rel="noopener noreferrer" target="_blank">Linkedin</a>
           <a href="mailto:piercefrank10@gmail.com">piercefrank10@gmail.com</a>
         </div>
 
@@ -18,9 +23,17 @@ const Menu = () => {
           <a href="https://www.spotlist.net/" rel="noopener noreferrer" target="_blank">Optioncent</a>
           <a href="https://www.medicle.net/" rel="noopener noreferrer" target="_blank">Medicle</a>
         </div>
+
+        <div className={styles.blogs}>
+          <h3>BLOGS</h3>
+          <a href="https://piercefrank10.medium.com/understanding-screen-108173a55474" rel="noopener noreferrer" target="_blank">Understanding Screen</a>
+          <a href="https://piercefrank10.medium.com/understanding-screen-108173a55474" rel="noopener noreferrer" target="_blank">Understanding Screen</a>
+        </div>
       </div>
 
       <div className={styles.bottom}>
+        <span className={styles.copyright}>&copy; Frank Pierce</span>
+        <span className={styles.close} onClick={() => setShowMenu(!showMenu)}>Close Menu</span>
       </div>
     </div>
   )
