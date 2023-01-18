40592321fabf1d70fd22e5007440669ecc2a27ee
llleeeaaannn <piercefrank10@gmail.com>
2023-01-18 23:12:35 +0000

    Added styling to project page, added link to navbar logo and extra styling

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,5 +1,6 @@
 import { AppContext } from '../App.js';
 import { useContext } from 'react';
+import { Link } from 'react-router-dom';
 import styles from '../styles/navbar.module.css';
 
 const Navbar = () => {
@@ -8,7 +9,7 @@ const Navbar = () => {
 
   return (
     <div className={styles.navbar}>
-      <span className={showMenu ? 'logo on' : 'logo'}>FP</span>
+      <Link to='/' onClick={() => setShowMenu(false)} className={showMenu ? 'logo on' : 'logo'}>FP</Link>
       <div className="navbarBurger" onClick={() => setShowMenu(!showMenu)}>
         <span className={ showMenu ? 'menuSelected' : '' }></span>
         <span className={ showMenu ? 'menuSelected' : '' }></span>
