994045eebaa250626814e1153335fb879fadbc3b
llleeeaaannn <piercefrank10@gmail.com>
2023-01-24 05:31:44 +0000

    Removed Typos

--- src/components/about.js
+++ src/components/about.js
@@ -6,7 +6,9 @@ const About = () => {
     <div className={styles.about}>
       <h2>About Me</h2>
       <h3>I love solving problems</h3>
-      <p>I recently graudated from Dublin City University with a Bachelors of Science in Enterprise Computing. I have spent the past months learnig about front-end development while creating web applications with real-world uses. I recently graudated from Dublin City University with a Bachelors of Science in Enterprise Computing. I have spent the past months learnig about front-end development while creating web applications with real-world uses. I recently graudated from Dublin City University with a Bachelors of Science in Enterprise Computing. I have spent the past months learnig about front-end development while creating web applications with real-world uses.</p>
+      <p>I'm a Front-end Developer from Dublin. I recently graduated from Dublin City University with a Bachelors of Science in Enterprise Computing.</p>
+      <p>I have spent the past months honing Front-end Development skills while creating web applications with real-world uses.</p>
+      <p>I pride myself on my initiative, being a fast learner and a great communicator</p>
     </div>
   )
 }
