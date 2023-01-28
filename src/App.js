b7efbe7948ef8ecfcbf2e20287dc81c2ead33438
llleeeaaannn <piercefrank10@gmail.com>
2023-01-28 13:09:31 +0000

    Added NPM scrollToTop package instead of custom script

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './styles/styles.css';
 import React, { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import ScrollToTop from './scrollToTop';
+import ScrollToTop from 'scroll-to-top-react-router';
 import Home from './views/home';
 import NotFound from './views/notfound';
 import Stockle from './views/projects/stockle';
